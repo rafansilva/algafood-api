7e4b3b39cfad0caf9873097b2221fa975fe4f4ab
Rafael N. Silva <rafaelnascimento0505@gmail.com>
2026-09-08 22:48:20 -0300

    .

---
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,45 @@
+package com.algaworks.algafood.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "cozinha")
+public class Cozinha {
+
+    @Id
+    private Long id;
+
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Cozinha cozinha = (Cozinha) o;
+        return Objects.equals(id, cozinha.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
